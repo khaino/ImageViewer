9762fd4d6eff7d9a735d7f2011b7fe450c622d92
Khaino <Pausian.Khai@nttdata.com>
2016-05-11 10:23:09 +0630

    add file for Collection View

---
+++ ImageViewer/IVGridViewController.h
@@ -0,0 +1,13 @@
+//
+//  IVGridViewController.h
+//  ImageViewer
+//
+//  Created by Khaino on 5/11/16.
+//  Copyright © 2016 khaino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IVGridViewController : UICollectionViewController
+
+@end
