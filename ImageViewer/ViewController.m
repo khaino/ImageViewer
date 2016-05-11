6d4eade37b6ccf6975403016465dbb4322987c64
Khaino <Pausian.Khai@nttdata.com>
2016-05-11 09:21:31 +0630

    first commit

--- ImageViewer/ViewController.m
+++ ImageViewer/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //this is to test git
 }
 
 - (void)didReceiveMemoryWarning {
