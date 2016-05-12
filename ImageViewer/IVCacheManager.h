4afd165c26e8cc787a15cf9e3983e027b074a8b8
Khaino <Pausian.Khai@nttdata.com>
2016-05-12 16:50:53 +0630

    add image cache manager

---
+++ ImageViewer/IVCacheManager.h
@@ -0,0 +1,13 @@
+//
+//  IVCacheManager.h
+//  ImageViewer
+//
+//  Created by Khaino on 5/12/16.
+//  Copyright © 2016 khaino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IVCacheManager : NSObject
+
+@end
---
+++ ImageViewer/IVCacheManager.m
@@ -0,0 +1,13 @@
+//
+//  IVCacheManager.m
+//  ImageViewer
+//
+//  Created by Khaino on 5/12/16.
+//  Copyright © 2016 khaino. All rights reserved.
+//
+
+#import "IVCacheManager.h"
+
+@implementation IVCacheManager
+
+@end
