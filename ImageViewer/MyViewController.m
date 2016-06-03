d4df3f18e9d73f68d6ab35c0b2a32e063e9dd54e
ncaung <2006nyeinchan@gmail.com>
2016-06-03 15:34:04 +0630

    Add doubble tap zoom to MyViewController. Add animated zoom out when slide.

--- ImageViewer/MyViewController.m
+++ ImageViewer/MyViewController.m
@@ -16,10 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // Image download activity indicator
     self.activityIndicator.hidesWhenStopped = YES;
     self.pageScrollView.delegate = self;
     [self.activityIndicator startAnimating];
+    
     // Do any additional setup after loading the view from its nib.
+    
+    // Add double touch gesture to pageScrollView
+    UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleDoubleTap:)];
+    doubleTap.numberOfTapsRequired = 2;
+    [self.pageScrollView addGestureRecognizer:doubleTap];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,9 +36,34 @@
     // Dispose of any resources that can be recreated.
 }
 
+// Set imageView as object to zoom
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
     return self.imageView;
 }
+
+// When slide to another image, image zoom is reset.
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    [self.pageScrollView setZoomScale:0.1f animated:YES];
+}
+
+// Double tap handler
+- (void)handleDoubleTap:(CGPoint)touchPoint {
+
+    // Cancel any single tap handling
+    [NSObject cancelPreviousPerformRequestsWithTarget:self.imageView];
+    
+    // If image is zoomed,
+    if (self.pageScrollView.zoomScale == self.pageScrollView.maximumZoomScale) {
+        
+        // image is back to normal
+        [self.pageScrollView setZoomScale:self.pageScrollView.minimumZoomScale animated:YES];
+    } else {
+        
+        // If image is not zoomed, zooming happens
+        [self.pageScrollView setZoomScale:self.pageScrollView.maximumZoomScale animated:YES];
+    }
+}
+
 /*
 #pragma mark - Navigation
 
